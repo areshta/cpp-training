491df625f0a341604ca62bc9eda044afbb2f7fff
Alex Reshta <alex.reshta@gmail.com>
2018-12-02 19:30:57 +0200

    Level 1. Hello. Console IO

---
+++ level1/examples/01_Hello/src/01_Hello.cpp
@@ -0,0 +1,11 @@
+/*
+ * The first program
+ */
+#include <iostream>
+using namespace std;
+
+int main() {
+	// Print to console
+	cout << "Hello World!" << endl;
+	return 0;
+}
---
+++ level1/examples/02_Consol_IO/src/Console_IO.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+int main() {
+
+	// The good practice for big projects:
+	// we did not open std namespace
+	// and use :: operator for access it
+
+	std::string name;
+	std::cout << "Enter your name: ";
+	std::cin >> name ;
+	std::cout << "You entered: " << name << std::endl;
+	std::cin.ignore(); // remove end of line symbol
+
+	std::string line;
+	std::cout << "Enter your first and second name: ";
+	std::getline(std::cin, line);
+	std::cout << "You entered: " << line << std::endl;
+
+	return 0;
+}
