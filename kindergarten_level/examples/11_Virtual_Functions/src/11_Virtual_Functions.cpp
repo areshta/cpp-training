722a80cddd2b68ea2aef7780b57af1b43a0183e9
Your Name <you@example.com>
2019-02-12 19:43:33 +0200

    minor update

--- kindergarten_level/examples/11_Virtual_Functions/src/11_Virtual_Functions.cpp
+++ kindergarten_level/examples/11_Virtual_Functions/src/11_Virtual_Functions.cpp
@@ -51,10 +51,12 @@ int main() {
 
 /*
     Task "Herarhy-4": Get previous project ("Herarhy-3")
-     	  and break apart to files:
-     	  1) heder -> for classes declaration
-     	  2) cpp-1 -> for classes definition
-     	  3) cpp-2 -> for classes definition
+    1) add other child class
+    2) add some virtual function in the parent and children classes
+    3) create array of pointers of the parent class type that point children objects
+    4) call virual functions in some loop
+    5) free objects memory
+
 */
 
 // Copyfree (F) By Oleksii Reshta (areshta). No rights reserved.
