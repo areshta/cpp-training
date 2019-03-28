b5bd175479868530a82358c95eb3073d14436973
Alexey Reshta <areshta@luxoft.com>
2019-03-28 19:59:25 +0200

    minor update

--- school_level/examples/03_sync_object/src/03_sync_object.cpp
+++ school_level/examples/03_sync_object/src/03_sync_object.cpp
@@ -11,6 +11,7 @@ ofstream *pOf;
 mutex mt;
 std::condition_variable ready;
 mutex mt_ready;
+bool b_note =  false; //for spurious wakeup preventing
 
 void log_to_file(const string &str ){
 		lock_guard<mutex> lg(mt);
@@ -20,16 +21,19 @@ int main() {
 
 	auto loger = async(launch::async,[](){
 					pOf =  new ofstream("lofile.txt");
+					b_note = true;
 					ready.notify_all();
 	    			return 1;});
 	auto user1 = async(launch::async,[](){
 					std::unique_lock<std::mutex> locker(mt_ready);
-					ready.wait(locker);
+					while(!b_note)
+						ready.wait(locker);
 					log_to_file("user1");
 	    			return 1;});
 	auto user2 = async(launch::async,[](){
 					std::unique_lock<std::mutex> locker(mt_ready);
-					ready.wait(locker);
+					while(!b_note)
+						ready.wait(locker);
 					log_to_file("user2");
 	    			return 1;});
 	user1.get();
